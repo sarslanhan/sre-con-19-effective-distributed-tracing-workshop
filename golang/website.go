36a1e8a36adb60fa0bc4eb579e0ca9b5b5a399b9
Serbay Arslanhan <serbay.arslanhan@zalando.de>
2019-10-02 21:22:18 +0100

    Drop instrumentation for the website

--- golang/website.go
+++ golang/website.go
@@ -6,15 +6,11 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	opentracing "github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
-	"github.com/opentracing/opentracing-go/log"
 	"github.com/pkg/errors"
 )
 
 // Website manage multiple web apps inside to serve web app with cart api
 type website struct {
-	tracer       opentracing.Tracer
 	engine       *gin.Engine
 	faultManager *FaultInjectionManager
 	az           int
@@ -31,10 +27,8 @@ func newWebsite(faultManager *FaultInjectionManager, instanceID string, az int) 
 	server.LoadHTMLFiles("templates/index.html")
 	server.StaticFile("/magic.js", "templates/magic.js")
 
-	t := setupTracer(siteAppName)
 	w := &website{
 		engine:       server,
-		tracer:       t,
 		faultManager: faultManager,
 		az:           az,
 		instanceID:   instanceID,
@@ -56,38 +50,19 @@ func (w *website) renderPage(c *gin.Context) {
 func (w *website) buyStuff(c *gin.Context) {
 	sku := c.PostForm("sku")
 
-	req := c.Request
-	ctx, err := w.tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(req.Header))
-	var span opentracing.Span
-	if err != nil {
-		span = w.tracer.StartSpan(buyStuffOperation)
-	} else {
-		span = w.tracer.StartSpan(buyStuffOperation, opentracing.ChildOf(ctx))
-	}
-	defer span.Finish()
-	ext.SpanKindRPCServer.Set(span)
-	span.SetTag(instanceIDTag, w.instanceID)
-
 	if len(sku) < 1 {
-		ext.Error.Set(span, true)
-		err := errors.New("missing SKU")
-		span.LogFields(log.Error(err))
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "no SKUs are present in the request"})
 		return
 	}
 
-	w.complexBusinessLogic(span)
+	w.complexBusinessLogic()
 
-	if err = w.storeRecordsInRemoteStorage(span); err != nil {
-		ext.Error.Set(span, true)
-		span.LogFields(log.Error(err))
+	if err := w.storeRecordsInRemoteStorage(); err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "could not store records into the storage"})
 		return
 	}
 
-	if err := w.addToCart(sku, span); err != nil {
-		ext.Error.Set(span, true)
-		span.LogFields(log.Error(err))
+	if err := w.addToCart(sku); err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "could not add the item to the cart"})
 		return
 	}
@@ -95,23 +70,11 @@ func (w *website) buyStuff(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("%s was added to the cart", sku)})
 }
 
-func (w *website) addToCart(sku string, parent opentracing.Span) error {
-	span := w.tracer.StartSpan(addToCartOperation, opentracing.ChildOf(parent.Context()))
-	defer span.Finish()
-	ext.SpanKindRPCClient.Set(span)
-
+func (w *website) addToCart(sku string) error {
 	url := fmt.Sprintf("http://localhost:8085/cart/%s", sku)
 	req, err := http.NewRequest(http.MethodPut, url, nil)
 	if err != nil {
 		return err
-	}
-
-	err = w.tracer.Inject(
-		span.Context(),
-		opentracing.HTTPHeaders,
-		opentracing.HTTPHeadersCarrier(req.Header))
-	if err != nil {
-		w.log(fmt.Sprintf("could't inject context, continuing without : %v", err))
 	}
 
 	res, err := http.DefaultClient.Do(req)
@@ -129,17 +92,11 @@ func (w *website) addToCart(sku string, parent opentracing.Span) error {
 	return nil
 }
 
-func (w *website) complexBusinessLogic(parent opentracing.Span) {
-	span := w.tracer.StartSpan(doComplexBusinessLogicOperation, opentracing.ChildOf(parent.Context()))
-	defer span.Finish()
+func (w *website) complexBusinessLogic() {
 	w.faultManager.sleepForAWhile(siteAppName)
 }
 
-func (w *website) storeRecordsInRemoteStorage(parent opentracing.Span) error {
-	span := w.tracer.StartSpan(storeRecordsOperation, opentracing.ChildOf(parent.Context()))
-	defer span.Finish()
-	ext.SpanKindRPCClient.Set(span)
-
+func (w *website) storeRecordsInRemoteStorage() error {
 	return w.faultManager.maybeFailTheOperation(siteAppName)
 }
 
